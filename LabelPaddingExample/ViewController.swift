3370a4b00696aa87cb6f7cc528aebc20e5598623
John Codeos <johncodeos@gmail.com>
2020-09-28 18:16:05 +0300

    first commit

--- LabelPaddingExample/ViewController.swift
+++ LabelPaddingExample/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // paddingLabelProgrammatically()
     }
 
+    // Programmatically
+    func paddingLabelProgrammatically() {
+        let label = PaddingLabel()
+        label.backgroundColor = .black
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 26, weight: .semibold)
+        label.text = "UILabel with Padding"
+        self.view.addSubview(label)
 
+        // Padding
+        label.paddingLeft = 15
+        label.paddingRight = 15
+        label.paddingTop = 8
+        label.paddingBottom = 8
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
 }
-
