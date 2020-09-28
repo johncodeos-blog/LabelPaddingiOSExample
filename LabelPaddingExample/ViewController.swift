a5716e33c2c838662a861a98a9ec310dd04666a5
John Codeos <johncodeos@gmail.com>
2020-09-28 13:45:36 +0300

    Initial Commit

---
+++ LabelPaddingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabelPaddingExample
+//
+//  Created by John Codeos on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
